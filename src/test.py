9a79645706ac7db5c563a52700109f2b1572cfc3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-10-13 16:00:11 +0900

    Googleのページタイトルを取ってくるテスト。 参考：https://hodalog.com/use-selenium-on-docker/

--- src/test.py
+++ src/test.py
@@ -1,1 +1,17 @@
-print("Success")
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+chrome_path = '/usr/bin/chromium-browser'
+chromedriver_path = '/usr/lib/chromium/chromedriver'
+o = Options()
+o.binary_location = '/usr/bin/chromium-browser'
+o.add_argument('--headless')
+o.add_argument('--disable-gpu')
+o.add_argument('--no-sandbox')
+o.add_argument('--window-size=1200x600')
+
+d = webdriver.Chrome(chromedriver_path, options=o)
+d.get('https://www.google.com')
+print(d.title)
+d.quit()
